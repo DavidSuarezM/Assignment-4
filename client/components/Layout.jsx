cc642a21b2d2ed3a0944758ab352ed51b0333104
David Suarez <david.suarezmolina@hotmail.com>
2025-06-29 14:41:33 -0400

    first commit

---
+++ client/components/Layout.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../src/assets/dsLogo.png';
+
+export default function Layout() {
+  return (
+    <>
+      <header>
+        <img src={logo} alt="Logo" height="100" width="100"></img>
+        <h1> My Portfolio</h1>
+      </header>
+
+      <link rel="icon" href="favicon.ico" type="image/x-icon"></link>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/services">Services</Link>| <Link to="/project">Project</Link>| <Link to="/contact">Contact</Link>
+      </nav>
+      <br />
+      <hr />
+
+    </>
+
+  );
+}
---
+++ client/src/about.jsx
@@ -0,0 +1,22 @@
+import formal from './assets/FormalPhoto.JPG';
+import resume from './assets/Resume DS.pdf';
+
+
+export default function About() {
+  return (
+    <>
+            <body>
+
+                <div class="container">
+                    <h2>🧑‍💻 About Me</h2>
+                    <img src={formal} alt="David Suarez" class="profileImage" height="200" width="200"></img>
+                        <p><strong>Name:</strong> David Andres Suarez Molina</p>
+                        <p>I'm an international student from Ecuador, currently living in Toronto, Canada. I love coding, learning new programming languages, and everything related to technology. I also love the world of networks, which is why I studied Electronic Engineering with a focus on Telecommunications at the Salesian University of Ecuador. In Ecuador, I worked in the telecommunications field, installing structured cabling and laying GPON networks. I also taught networks and telecommunications, as a Cisco instructor.</p>
+                        <a href={resume} target="_blank">📄 Check it out my resume!</a>
+                </div>
+
+            </body>
+        </>
+
+  );
+}
